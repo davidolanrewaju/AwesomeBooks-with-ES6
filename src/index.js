9af267c9a554f430ea6743bba98641c3a120f3d1
Dave <davidolanrewaju538@gmail.com>
2023-04-19 09:26:26 +0100

    Update file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import BookList from './modules/update-booklist.js';
+import BookList from '../modules/update-booklist.js';
 
 import {
   showBookList,
   showAddBook,
   showContact,
-} from './modules/navigation.js';
+} from '../modules/navigation.js';
 
-import DateDisplay from './modules/date.js';
+import DateDisplay from '../modules/date.js';
 
 // eslint-disable-next-line no-unused-vars
 const bookList = new BookList('.book-lists', '#form', 'bookLists');
